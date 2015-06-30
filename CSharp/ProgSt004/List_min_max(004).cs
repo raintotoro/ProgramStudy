1725e44ff15ca68c8edf904976b777de2c370f47
raintotoro <ryuumutoro757@gmali.com>
2015-06-30 16:09:24 +0900

    second commit!

---
+++ CSharp/ProgSt004/List_min_max(004).cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List_min_max
+{
+    public static void Main(string[] args)
+    {
+        var l = new List<int>() { 7, 3, 5, 6, 2, 1, 4 };
+        int[] ary = l.ToArray();
+        int max = int.MinValue;
+        int min = int.MaxValue;
+        foreach (var i in ary)
+        {
+            min = Math.Min(min, i);
+            max = Math.Max(max, i);
+        }
+        Console.WriteLine("最小値は｢{0}｣ 最大値は｢{1}｣です.", min, max);
+        Console.ReadKey();
+    }
+}
---
+++ CSharp/ProgSt005/Sort(005).cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Sort_005_ 
+{
+    public static void Main(string[] args)
+    {
+        var numArray = new List<int>();
+        Console.WriteLine("終了する場合は｢0｣を入力してください :");
+        while (true)
+        {
+            string s = Console.ReadLine();
+            int i = int.Parse(s);
+            if (i == 0)
+            {
+                break;
+            }
+            numArray.Add(i);
+        }
+        int[] ary = numArray.ToArray();
+        // 昇順
+        Array.Sort(ary);
+        Console.WriteLine("昇順 :");
+        foreach (var n in ary)
+        {
+            Console.Write(n);
+        }
+        
+        Console.WriteLine("");
+        // 降順
+        Console.WriteLine("降順 :");
+        Array.Reverse(ary);
+        foreach (var s in ary)
+        {
+            Console.Write(s);
+        }
+        Console.ReadKey();
+    }
+}
+
