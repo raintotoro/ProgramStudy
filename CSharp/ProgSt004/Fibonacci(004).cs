8b2fd3944ae960ca1d8aa81cdcb40b7825cfa70a
raintotoro <ryuumutoro757@gmail.com>
2015-06-30 19:58:19 +0900

    thrid commit!

---
+++ CSharp/ProgSt004/Fibonacci(004).cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class hoge
+{
+    static void Main(string[] args)
+    {
+        Fibonucci f = new Fibonacci();
+        // F(0)..F(39)までを列挙する
+        // オーバーフローする限界はF(46)
+        foreach (var i in f.GetSequence().Take(47))
+        {
+            Console.WriteLine(i.ToString());
+        }
+        Console.ReadLine();
+    }
+}
+
+class Fibonacci
+{
+    int[] array = new int[] { 0, 1 };
+    public IEnumerable<int> GetSequence()
+    {
+        yield return 0;
+        yield return 1;
+
+        while (true)
+        {
+            int fibo = array[0] + array[1];
+            Save(fibo);
+            yield return fibo;
+        }
+    }
+    private void Save(int n)
+    {
+        array[0] = array[1];
+        array[1] = n;
+    }
+}
