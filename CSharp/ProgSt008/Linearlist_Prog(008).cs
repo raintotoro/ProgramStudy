3968a8e425528668bdc49358b632e856ffa3bac2
raintotoro <ryuumutoro757@gmail.com>
2015-07-12 21:49:33 +0900

    Update3 incomplete 008

---
+++ CSharp/ProgSt008/Linearlist_Prog(008).cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Linearlist_Prog
+{
+    static void Main(string[] args)
+    {
+        var myList = new MyList<string>();
+
+        // リストに(ノード)追加が可
+        var list1 = myList.Add("raintotoro").Add("am").Add("I");
+        var list2 = myList.Add("I").Add("am").Add("hoge");
+
+        Console.WriteLine(list1);
+        // Get(数字)でリストのデータを指定可能
+        Console.WriteLine(list1.Get(2));
+
+        list2 = Reverse(list2);
+        Console.WriteLine(list2);
+        Console.WriteLine();
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+
+    public int[] Reverse(int[] val)
+    {
+        int len = val.Length;
+        int[] result = new int[len];
+        for (int i = 0; i < len; i++)
+        {
+            result[i] = val[len - 1 - i];
+        }
+        return result;
+    }
+
+}
+
---
+++ CSharp/ProgSt008/MyList(008).cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class MyList<T>
+{
+    T val;
+    MyList<T> prev;
+
+    MyList(T val, MyList<T> prev)
+    {
+        this.val = val;
+        this.prev = prev;
+    }
+
+    public MyList() { }
+
+    public MyList<T> Add(T value)
+    {
+        return new MyList<T>(value, this);
+    }
+
+    public bool IsEmpty()
+    {
+        return prev == null;
+    }
+
+    public int Size()
+    {
+        if(IsEmpty ()) return 0;
+        else return prev.Size() +1;
+    }
+
+    public T Get(int index)
+    {
+        if (index == 0) return val;
+        else return prev.Get(index - 1);
+    }
+   
+    public override string ToString()
+    {
+        if (IsEmpty()) return ""; 
+        else return val.ToString() + " " + prev.ToString();
+    }
+
+}
+
+
+
